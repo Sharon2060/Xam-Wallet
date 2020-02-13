1cc3c347473c4520af5c55a6cc7caaf0d4c4793d
sharon Sarbah <sharon Sarbah@LAPTOP-CTG1MPVG>
2020-02-13 11:52:46 +0000

    Model Initial commit

---
+++ XamWallet/XamWallet/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamWallet.Models
+{
+    class User
+    {
+        public int UserID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String PhoneNumber { get; set; }
+    }
+}
