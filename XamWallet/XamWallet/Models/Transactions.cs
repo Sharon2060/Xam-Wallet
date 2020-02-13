f2e0b47f355068bdd40cb1f3f812b976cd043f03
sharon Sarbah <sharon Sarbah@LAPTOP-CTG1MPVG>
2020-02-13 12:09:34 +0000

    Added Transactions Model

---
+++ XamWallet/XamWallet/Models/Transactions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamWallet.Models
+{
+    class Transactions
+    {
+        public int TransactionID { get; set; }
+
+        public String SenderID { get; set; }
+        public String ReceiverID { get; set; }
+    }
+}
